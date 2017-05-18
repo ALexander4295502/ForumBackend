07a59b75474c8def001d163e27fc9e39669b0537
ALexander4295502 <yuanzhengstl@gmail.com>
2017-05-18 00:31:20 -0500

    test

--- app.js
+++ app.js
@@ -82,5 +82,5 @@ app.use(function(err, req, res, next) {
 // finally, let's start our server...
 var server = app.listen( process.env.PORT || 3000, "localhost", function(){
   console.log('Listening on port ' + server.address().port);
-  console.log('Server running at http://' + server.address().address + ':' + server.address().port);
+  console.log('Server running at ' + process.env.MONGODB_URI);
 });
