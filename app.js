f460dd7b11f07027f7ce8d872f0bccd2ab360e6e
ALexander4295502 <yuanzhengstl@gmail.com>
2017-05-18 09:41:35 -0500

    test

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ app.use(function(err, req, res, next) {
 });
 
 // finally, let's start our server...
-var server = app.listen( process.env.PORT || 3000, "localhost", function(){
+var server = app.listen( process.env.PORT || 3000, function(){
   console.log('Listening on port ' + server.address().port);
   console.log('Server running at ' + server.address().address);
 });
