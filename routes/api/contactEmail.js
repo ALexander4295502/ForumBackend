b618ac6d07067a2eff669b31dd30daab5b613271
ALexander4295502 <yuanzhengstl@gmail.com>
2017-09-12 20:20:19 -0500

    add a route to handle contact message and use mailgun to send messages.

--- routes/api/contactEmail.js
+++ routes/api/contactEmail.js
@@ -4,9 +4,10 @@
 var router = require('express').Router();
 var Mailgun = require('mailgun-js');
 
-var mailgun_api_key = 'key-41e1018166391483ab62350f08ece833';
-var mailgun_domain = 'mail.rourouzhuzhu.com';
-var mailgun_from_who = 'Do Not Reply <email@zheng.town>';
+const mailgun_api_key = 'key-41e1018166391483ab62350f08ece833';
+const mailgun_domain = 'mail.rourouzhuzhu.com';
+const mailgun_from_who = 'Do Not Reply <email@zheng.town>';
+const Admin_email = 'yuanzhengstl@gmail.com';
 
 router.post('/', function(req, res, next) {
   var contactInfo = {
@@ -14,58 +15,44 @@ router.post('/', function(req, res, next) {
     email: req.body.email,
     message: req.body.message
   };
-
+  console.log(req.body.name);
   var mailgun = new Mailgun({apiKey: mailgun_api_key, domain: mailgun_domain});
 
-  mailgun.validate(req.body.email, function (err, body) {
-    if(err) {
+  const data = {
+    //Specify email data
+    from: mailgun_from_who,
+    //The email to contact
+    to: Admin_email,
+    //Subject and text data
+    subject: 'Message from portfolio contact page',
+    html: `Hi, my name is ${contactInfo.name}. <br/><br/>
+           Here is the content of my message: <br/>
+           ${contactInfo.message} <br/><br/>`+
+    `This is my contact email:</p><p>${contactInfo.email}</p>`,
+    text: `Hi, my name is ${contactInfo.name}. \n\n
+           Here is the content of my message: \n
+           ${contactInfo.message} \n\n`+
+           `This is my contact email: ${contactInfo.email} `
+  };
+
+  mailgun.messages().send(data, function (err, body) {
+    if(err){
+      console.log(err);
       return res.status(422).json({
-        errors: Object.keys(err.errors).reduce(function (errors, key) {
-          errors[key] = err.errors[key].message;
+        errors: Object.keys(err).reduce(function (errors, key) {
+          errors[key] = err[key];
           return errors;
         }, {})
       });
     }
-    console.log(body);
-    return res.json({
-      msg: "Email validation result",
-      info: body
-    });
+    else {
+      console.log(body);
+      return res.json({
+        msg: 'Your message has been sent to Zheng.',
+        info: body
+      });
+    }
   });
-
-  // var data = {
-  //   //Specify email data
-  //   from: mailgun_from_who,
-  //   //The email to contact
-  //   to: user.email,
-  //   //Subject and text data
-  //   subject: 'Message from portfolio contact page',
-  //   html: `This is my contact email:</p><p>${contactInfo.email}</p>`,
-  //   text: `Hi, my name is ${contactInfo.name} \n
-  //          Here is the content of my message: \n
-  //          ${contactInfo.message}`
-  // };
-  //
-  // mailgun.messages().send(data, function (err, body) {
-  //   if(err){
-  //     console.log(err);
-  //     if(err.name === 'ValidationError'){
-  //       console.log("validationError!");
-  //       return res.status(422).json({
-  //         errors: Object.keys(err.errors).reduce(function (errors, key) {
-  //           errors[key] = err.errors[key].message;
-  //           return errors;
-  //         }, {})
-  //       });
-  //     }
-  //   }
-  //   else {
-  //     return res.json({
-  //       msg: 'An email has been sent to you. Please check it to verify your account.',
-  //       info: body
-  //     });
-  //   }
-  // });
 
 
 });
